a06858f5b6ca6d831ecaca03928e17e6d8314587
Man Nguyen <man.nguyen@botstar.com>
2021-10-26 23:39:26 +0700

    update hardhat config

--- nft/hardhat.config.js
+++ nft/hardhat.config.js
@@ -6,18 +6,17 @@ require("@nomiclabs/hardhat-waffle");
  */
 const ALCHEMY_API_KEY = "v-X5XjgicGy-5HzpBRyLPTZTcuDh91-N";
 
-const ROPSTEN_PRIVATE_KEY = "a906a48b7b62a03289635ba4480231f209c9fad4ba615ee058e583ee03e3e5ca";
+const RINKEBY_PRIVATE_KEY = "a906a48b7b62a03289635ba4480231f209c9fad4ba615ee058e583ee03e3e5ca";
 
 module.exports = {
     solidity: "0.7.3",
-    defaultNetwork: "ropsten",
+    defaultNetwork: "rinkeby",
     networks: {
         hardhat: {
         },
-        ropsten: {
-            url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-            accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+        rinkeby: {
+            url: https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY},
+            accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
         }
     }
 };
-
